1ee3f1ccbc6f5b63f035222afefd475dab2a15c2
FordonN <fn.alin.business@gmail.com>
2022-05-06 06:03:29 +0300

    remote unused files

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,3 @@
-export interface ResponseData<T> {
-    page: number
-    total_pages: number
-    total_results: number
-    results: T
-}
-export interface QueryOptions {
-    retry?: boolean
-    cacheTime?: number
-    staleTime?: number
-    enabled?: boolean
-    refetchInterval?: number | false
-    refetchOnMount?: boolean | 'always'
-    refetchOnWindowFocus?: boolean | 'always'
-}
 export interface CheckButtonType {
     label: string
     checked: boolean
